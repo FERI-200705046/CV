794ef582bca5866ded3b2919e01847ef190f1b3c
FERI-200705046 <107051672+FERI-200705046@users.noreply.github.com>
2022-11-11 16:12:30 +0700

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resume/CV Design</title>
+	<link rel="stylesheet" href="{{asset('stylescv.css')}}">
+  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="resume">
+   <div class="resume_left">
+     <div class="resume_profile">
+       <img src="{{asset('profile.jpg')}}" alt="profile_pic">
+     </div>
+     <div class="resume_content">
+       <div class="resume_item resume_info">
+         <div class="title">
+           <p class="bold">FERI SAPUTRA</p>
+           <p class="regular">Gamer | Freelane</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fas fa-map-signs"></i>
+             </div>
+             <div class="data">
+               Aceh <br /> Indonesia
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-mobile-alt"></i>
+             </div>
+             <div class="data">
+               +082236437418
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-envelope"></i>
+             </div>
+             <div class="data">
+               saputraferry954@gmail.com
+             </div>
+           </li>
+           
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">skill's</p>
+         </div>
+         <ul>
+           <li>
+             <div class="skill_name">
+               HTML
+             </div>
+             <div class="skill_progress">
+               <span style="width: 80%;"></span>
+             </div>
+             <div class="skill_per">80%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               CSS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 70%;"></span>
+             </div>
+             <div class="skill_per">70%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Corel
+             </div>
+             <div class="skill_progress">
+               <span style="width: 90%;"></span>
+             </div>
+             <div class="skill_per">90%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               JS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 60%;"></span>
+             </div>
+             <div class="skill_per">60%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               PHP
+             </div>
+             <div class="skill_progress">
+               <span style="width: 88%;"></span>
+             </div>
+             <div class="skill_per">88%</div>
+           </li>
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">Social</p>
+         </div>
+         <ul>
+          <li>
+            <li>      
+            <div class="skill_name">
+                    Indonesia
+                  </div>
+                  <div class="skill_progress">
+                    <span style="width: 80%;"></span>
+                  </div>
+                  <div class="skill_per">80%</div>
+                </li>
+              <li>
+                <div class="skill_name">
+                  English
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 70%;"></span>
+                </div>
+                <div class="skill_per">70%</div>
+              </li>
+              <li>
+                <div class="skill_name">
+                  Aceh
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 70%;"></span>
+                </div>
+                <div class="skill_per">70%</div>
+              </li>
+          </li>
+        </ul>
+       </div>
+     </div>
+  </div>
+  <div class="resume_right">
+    <div class="resume_item resume_about">
+        <div class="title">
+           <p class="bold">About us</p>
+         </div>
+        <p>My Name is FERI SAPUTRA, I'm 21 Years Old, I From Banda Aceh, Aceh, Indonesia</p>
+        <p>I Graduated from University Of Ar-raniry From Major Information Tehcnology</p>
+        <p>My Hoby is Playing Games, Watching movie, and Eat</p>
+    </div>
+    <div class="resume_item resume_work">
+        <div class="title">
+           <p class="bold">Education</p>
+         </div>
+        <ul>
+            <li>
+                <div class="date">2003-2009</div> 
+                <div class="info">
+                     <p class="semi-bold">SDN Cotmee</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2009-2012</div>
+              <div class="info">
+                     <p class="semi-bold">SMPN 5 Kuala</p> 
+                  
+                </div>
+            </li>
+            <li>
+              <div class="date">2012 - 2015</div>
+              <div class="info">
+                     <p class="semi-bold">SMKN 2 Meulaboh</p> 
+                  <p>Major: Teknik Kendaraan Ringan</p>
+                </div>
+            </li>
+            <li>
+              <div class="date">2015 - 2020</div>
+              <div class="info">
+                     <p class="semi-bold">Islamic University Of Ar-raniry, Banda Aceh</p> 
+                  <p>Major: Bachelor Computer Engineering</p>
+                </div>
+            </li>
+            
+          </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_education">
+      <div class="title">
+           <p class="bold">Work Experience</p>
+         </div>
+      <ul>
+            <li>
+                <div class="date">2020</div> 
+                <div class="info">
+                     <p class="semi-bold">Student</p> 
+                  
+                </div>
+            </li>
+            
+        </ul>
+    </div>
+    <div class="resume_item resume_hobby">
+      <div class="title">
+           <p class="bold">Hobby</p>
+         </div>
+       <ul>
+         <li><i class="fas fa-book"></i></li>
+         <li><i class="fas fa-gamepad"></i></li>
+         <li><i class="fas fa-music"></i></li>
+         <li><i class="fas fa-basketball-ball"></i></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
